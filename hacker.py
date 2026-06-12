1785baa709e26f5ec2fbabe1d212d226beeacbaa
adt-thp1905 <aadityathapliyal03@gmail.com>
2026-06-12 12:21:56 +0530

    Update hacker.py

--- hacker.py
+++ hacker.py
@@ -2,7 +2,7 @@ t = input("Enter S.no.")
 x = input("Enter event here")
 y = input("Enter date here")
 z = input("Enter time here")
-with open("todo.pdf" ,"w") as f:
+with open("todo.txt" ,"w") as f:
  f.write(f"{t} . {x} {y} {z}")
  for o in f:
   f.append(f"{t} . {x} {y} {z}")
