931c2b39a6407dc7a4fabff8ee72adc1283d8a1d
adt-thp1905 <aadityathapliyal03@gmail.com>
2026-06-12 12:36:26 +0530

    Update hacker.py

--- hacker.py
+++ hacker.py
@@ -1,6 +1,7 @@
-t = input("Enter S.no.")
-x = input("Enter event here")
-y = input("Enter date here")
-z = input("Enter time here")
-with open("todo.txt" , "a") as p:
+def todo():
+ t = input("Enter S.no.")
+ x = input("Enter event here")
+ y = input("Enter date here")
+ z = input("Enter time here")
+ with open("todo.txt" , "a") as p:
   p.write(f"{t} . {x} {y} {z}"/n)
