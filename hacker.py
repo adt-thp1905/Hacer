4085e055e39a05f9285330e86ab8cf408e2f6e27
adt-thp1905 <aadityathapliyal03@gmail.com>
2026-06-12 12:19:36 +0530

    Update hacker.py

--- hacker.py
+++ hacker.py
@@ -3,6 +3,6 @@ x = input("Enter event here")
 y = input("Enter date here")
 z = input("Enter time here")
 with open("todo.pdf" ,"w") as f:
- for x in f:
+ for o in f:
   f.write(f"{t} . {x} {y} {z}")
  
